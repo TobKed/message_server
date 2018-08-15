5d2f89fe3a40e20f0d907448c2552c41703f0991
Tobiasz Kedzierski <tobiaszkedzierski@gmail.com>
2018-08-15 14:33:27 +0200

    add URI variables

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,8 +1,12 @@
 import psycopg2
 import os
 
+DB_NAME = "msgs_server"
+DB_URI = os.environ.get('SERVER_DB_URI')  # postgresql://postgres@localhost
+DB_COMPLETE_URI = "/".join([DB_URI, DB_NAME])
 
-def create_db(db_name, db_uri):
+
+def create_db(db_name=DB_NAME, db_uri=DB_URI):
     with psycopg2.connect(db_uri) as db_con:
         db_con.autocommit = True
         sql = "CREATE DATABASE {}".format(db_name)
@@ -10,7 +14,7 @@ def create_db(db_name, db_uri):
             curs.execute(sql)
 
 
-def nuke_db(db_name, db_uri):
+def nuke_db(db_name=DB_NAME, db_uri=DB_URI):
     with psycopg2.connect(db_uri) as db_con:
         db_con.autocommit = True
         sql = "DROP DATABASE {}".format(db_name)
@@ -18,8 +22,8 @@ def nuke_db(db_name, db_uri):
             curs.execute(sql)
 
 
-def create_users_table(db_name, db_uri):
-    with psycopg2.connect(db_uri) as db_con:
+def create_users_table(db_complete_uri=DB_COMPLETE_URI):
+    with psycopg2.connect(DB_COMPLETE_URI) as db_con:
         sql = """CREATE TABLE Users(
             id SERIAL,
             email VARCHAR(255) NOT NULL UNIQUE,
@@ -32,10 +36,8 @@ def create_users_table(db_name, db_uri):
 
 
 if __name__ == '__main__':
-    db_name = "msgs_server"
-    DB_URI = os.environ.get('SERVER_DB_URI')   # postgresql://postgres@localhost
+    # create_db()
+    # create_users_table()
 
-    create_db(db_name=db_name, db_uri=DB_URI)
-    create_users_table(db_name=db_name, db_uri="/".join([DB_URI, db_name]))
-
-    # nuke_db(db_name=db_name, db_uri=DB_URI)
+    # nuke_db()
+    pass
