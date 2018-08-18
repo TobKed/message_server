d9f94c32001dceb73e13102ef1d333e6a1978843
Tobiasz Kedzierski <tobiaszkedzierski@gmail.com>
2018-08-18 15:39:47 +0200

    add create_messeges_table()

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -36,9 +36,26 @@ def create_users_table(db_complete_uri=DB_COMPLETE_URI):
             curs.execute(sql)
 
 
+def create_messeges_table(db_complete_uri=DB_COMPLETE_URI):
+    with psycopg2.connect(DB_COMPLETE_URI) as db_con:
+        sql = """CREATE TABLE Messeges(
+            id SERIAL,
+            from_id INTEGER NOT NULL,
+            to_id INTEGER NOT NULL,
+            msg_text TEXT,
+            creation_date TIMESTAMP NOT NULL DEFAULT NOW(),
+            PRIMARY	KEY(id),
+            FOREIGN	KEY(from_id) REFERENCES Users(id) ON DELETE CASCADE,
+            FOREIGN	KEY(to_id) REFERENCES Users(id) ON DELETE CASCADE
+        )"""
+        with db_con.cursor() as curs:
+            curs.execute(sql)
+
+
 if __name__ == '__main__':
     # nuke_db()
-    create_db()
-    create_users_table()
+    # create_db()
+    # create_users_table()
+    # create_messeges_table()
 
     pass
